e1303c98d483cb29189cd24e5fd9b6bc1f879abb
Miguel Korn <miguel.korn@hva.nl>
2021-11-02 10:38:10 +0100

    Fixed 2015:10, fixed #11

---
+++ 2015/day_10/index.ts
@@ -0,0 +1,33 @@
+import { assertEquals } from "https://deno.land/std@0.112.0/testing/asserts.ts";
+import { getEnabledParts, runAndTime } from "../../utils/utils.ts";
+
+const [partOneEnabled, partTwoEnabled, testsEnabled] = getEnabledParts()
+const input = await Deno.readTextFile(Deno.args[1])
+
+const partOne = (i: string, rounds = 1): number => {
+    return [...Array(rounds).keys()].reduce((str) => {
+        return (str.match(/(\d)\1*/g) ?? []).reduce((t, v) => `${t}${v.length}${v[0]}`, "")
+    }, i).length
+}
+const partTwo = (i: string): number => {
+    return partOne(i, 50)
+}
+
+// --- Tests
+
+partOneEnabled && testsEnabled && (() => {
+    assertEquals(partOne('1'), 2)
+    assertEquals(partOne('11'), 2)
+    assertEquals(partOne('21'), 4)
+    assertEquals(partOne('1211'), 6)
+    assertEquals(partOne('111221'), 6)
+})()
+
+partTwoEnabled && testsEnabled && (() => {
+    // assertEquals(partTwo(''), 0)
+})()
+
+// --- End Tests
+
+partOneEnabled && runAndTime('partOne', () => partOne(input, 40))
+partTwoEnabled && runAndTime('partTwo', () => partTwo(input))
