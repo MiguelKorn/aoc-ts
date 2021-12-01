6f9d89e3b9fef43f8bc27af4a1d74fd1498ea9b8
Miguel Korn <miguel.korn@hva.nl>
2021-12-01 09:49:29 +0100

    [2021:1] made solutions shorter

--- 2021/day_2/index.ts
+++ 2021/day_2/index.ts
@@ -5,38 +5,20 @@ const [partOneEnabled, partTwoEnabled, testsEnabled] = getEnabledParts()
 const input = await Deno.readTextFile(Deno.args[1])
 
 const partOne = (i: string): number => {
-    return i.split('\n').map(Number).reduce((s, v, i, a) => s + +!!(i > 0 && v > a[i - 1]), 0)
+    return 0
 }
 const partTwo = (i: string): number => {
-    return i.split('\n').map(Number).reduce((s, v, i, a) => s + +!!(i > 2 && v > a[i - 3]), 0)
+    return 0
 }
 
 // --- Tests
 
 partOneEnabled && testsEnabled && (() => {
-    assertEquals(partOne('199\n' +
-        '200\n' +
-        '208\n' +
-        '210\n' +
-        '200\n' +
-        '207\n' +
-        '240\n' +
-        '269\n' +
-        '260\n' +
-        '263'), 7)
+    assertEquals(partOne(''), 0)
 })()
 
 partTwoEnabled && testsEnabled && (() => {
-    assertEquals(partTwo('199\n' +
-        '200\n' +
-        '208\n' +
-        '210\n' +
-        '200\n' +
-        '207\n' +
-        '240\n' +
-        '269\n' +
-        '260\n' +
-        '263'), 5)
+    assertEquals(partTwo(''), 0)
 })()
 
 // --- End Tests
