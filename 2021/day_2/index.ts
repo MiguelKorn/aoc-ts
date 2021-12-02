f6e62479f70cb596afc99537eed4c213017b1e82
Miguel Korn <miguel.korn@hva.nl>
2021-12-02 15:18:59 +0100

    [2021:2] cleanup

--- 2021/day_2/index.ts
+++ 2021/day_2/index.ts
@@ -4,20 +4,25 @@ import { getEnabledParts, runAndTime } from "../../utils/utils.ts";
 const [partOneEnabled, partTwoEnabled, testsEnabled] = getEnabledParts()
 const input = await Deno.readTextFile(Deno.args[1])
 
+const parseLine = (line: string): [string, number] => {
+    const split = line.split(' ')
+    return [split[0].substr(0, 1), Number(split[1])]
+}
+
 const partOne = (i: string): number => {
     const pos = i.split('\n').reduce(([x, y], op) => {
-        const [d, v] = op.split(' ').map((n, i) => i > 0 ? +n : n.substr(0, 1)) as [string, number]
+        const [d, v] = parseLine(op)
         y += (d === 'u') ? -v : (d === 'd') ? v : 0
-        x += (d === 'f') ? v : (d === 'r') ? -v : 0
+        x += (d === 'f') ? v : 0
         return [x, y]
     }, [0, 0])
     return pos[0] * pos[1]
 }
 const partTwo = (i: string): number => {
     const pos = i.split('\n').reduce(([x, y, a], op) => {
-        const [d, v] = op.split(' ').map((n, i) => i > 0 ? +n : n.substr(0, 1)) as [string, number]
+        const [d, v] = parseLine(op)
         a += (d === 'u') ? -v : (d === 'd') ? v : 0
-        x += (d === 'f') ? v : (d === 'r') ? -v : 0
+        x += (d === 'f') ? v : 0
         y += (d === 'f') ? v * a : 0
         return [x, y, a]
     }, [0, 0, 0])
