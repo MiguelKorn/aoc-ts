dee9fb798637fcb9ee9634e56346b90bb9a9f0c7
Miguel Korn <miguel.korn@hva.nl>
2021-12-02 06:33:34 +0100

    [2021:2] initial solution

--- 2021/day_2/index.ts
+++ 2021/day_2/index.ts
@@ -5,20 +5,43 @@ const [partOneEnabled, partTwoEnabled, testsEnabled] = getEnabledParts()
 const input = await Deno.readTextFile(Deno.args[1])
 
 const partOne = (i: string): number => {
-    return 0
+    const pos = i.split('\n').reduce(([x, y], op) => {
+        const [d, v] = op.split(' ').map((n, i) => i > 0 ? +n : n.substr(0, 1)) as [string, number]
+        y += (d === 'u') ? -v : (d === 'd') ? v : 0
+        x += (d === 'f') ? v : (d === 'r') ? -v : 0
+        return [x, y]
+    }, [0, 0])
+    return pos[0] * pos[1]
 }
 const partTwo = (i: string): number => {
-    return 0
+    const pos = i.split('\n').reduce(([x, y, a], op) => {
+        const [d, v] = op.split(' ').map((n, i) => i > 0 ? +n : n.substr(0, 1)) as [string, number]
+        a += (d === 'u') ? -v : (d === 'd') ? v : 0
+        x += (d === 'f') ? v : (d === 'r') ? -v : 0
+        y += (d === 'f') ? v * a : 0
+        return [x, y, a]
+    }, [0, 0, 0])
+    return pos[0] * pos[1]
 }
 
 // --- Tests
 
 partOneEnabled && testsEnabled && (() => {
-    assertEquals(partOne(''), 0)
+    assertEquals(partOne('forward 5\n' +
+        'down 5\n' +
+        'forward 8\n' +
+        'up 3\n' +
+        'down 8\n' +
+        'forward 2'), 150)
 })()
 
 partTwoEnabled && testsEnabled && (() => {
-    assertEquals(partTwo(''), 0)
+    assertEquals(partTwo('forward 5\n' +
+        'down 5\n' +
+        'forward 8\n' +
+        'up 3\n' +
+        'down 8\n' +
+        'forward 2'), 900)
 })()
 
 // --- End Tests
