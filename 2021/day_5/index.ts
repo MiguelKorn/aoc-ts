7ceaf366ad0053e30f0a80ce0352b8f7f63610fb
Miguel Korn <miguel.korn@hva.nl>
2021-12-05 07:25:01 +0100

    [2021:5] cleanup solution

--- 2021/day_5/index.ts
+++ 2021/day_5/index.ts
@@ -4,38 +4,35 @@ import { getEnabledParts, runAndTime } from "../../utils/utils.ts";
 const [partOneEnabled, partTwoEnabled, testsEnabled] = getEnabledParts()
 const input = await Deno.readTextFile(Deno.args[1])
 
+const { max, min } = Math
+
 const partOne = (i: string, p2 = false): number => {
     return [...i.split('\n').reduce((a, l) => {
-        const [x1,y1,x2,y2] = l.match(/\d+/g)!.map(Number)
-        if(x1==x2) {
-            for (let j = 0; j <= Math.max(y2,y1)-Math.min(y1,y2); j++) {
-                const k = [x1, (y2> y1 ? y1 : y2) + j].join(':')
-                const c = a.get(k)
-                a.set(k, c ? c + 1 : 1)
+        let [x1, y1, x2, y2] = l.match(/\d+/g)!.map(Number)
+        if (x1 == x2) {
+            for (let j = min(y1, y2); j <= max(y1, y2); j++) {
+                const k = [x1, j].join(':')
+                a.set(k, (a.get(k) ?? 0) + 1)
             }
-        } else if(y1==y2) {
-            for (let j = 0; j <= Math.max(x2,x1)-Math.min(x1,x2); j++) {
-                const k = [(x2> x1 ? x1 : x2) + j, y1].join(':')
-                const c = a.get(k)
-                a.set(k, c ? c + 1 : 1)
+        } else if (y1 == y2) {
+            for (let j = min(x1, x2); j <= max(x1, x2); j++) {
+                const k = [j, y1].join(':')
+                a.set(k, (a.get(k) ?? 0) + 1)
             }
-        } else{
-            if(p2) {
-                let x = x1, y=y1
-                let k = [x2,y2].join(':')
-                let c = a.get(k)
-                a.set(k, c ? c + 1 : 1)
-                while (x!==x2 && y!==y2) {
-                    const k = [x,y].join(':')
-                    const c = a.get(k)
-                    a.set(k, c ? c + 1 : 1)
-                    x += x2>x1 ? 1 : -1
-                    y += y2>y1 ? 1 : -1
+        } else {
+            if (p2) {
+                let k = [x2, y2].join(':')
+                a.set(k, (a.get(k) ?? 0) + 1)
+                while (x1 !== x2 && y1 !== y2) {
+                    k = [x1, y1].join(':')
+                    a.set(k, (a.get(k) ?? 0) + 1)
+                    x1 += x2 > x1 ? 1 : -1
+                    y1 += y2 > y1 ? 1 : -1
                 }
             }
         }
         return a
-    }, new Map).values()].filter(k => k> 1).length
+    }, new Map).values()].filter(k => k > 1).length
 }
 const partTwo = (i: string): number => {
     return partOne(i, true)
