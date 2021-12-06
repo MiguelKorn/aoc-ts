5ccd226f4c323bd1c7c6d8f3f3b564c1fee1a84c
Miguel Korn <miguel.korn@hva.nl>
2021-12-06 07:32:40 +0100

    [2021:6] cleanup

--- 2021/day_6/index.ts
+++ 2021/day_6/index.ts
@@ -5,15 +5,6 @@ const [partOneEnabled, partTwoEnabled, testsEnabled] = getEnabledParts()
 const [input, input2] = await getInput()
 
 const partOne = (i: string, d: number = 80): number => {
-    const f = i.split(",").map(Number)
-    const x = [...Array(d).keys()].reduce((a, i) => {
-        const z = a.filter(n => n === 0).length
-        return [...a.map(n => n === 0 ? 6 : n - 1), ...(Array(z).fill(8))]
-    }, f)
-    return x.length
-}
-const partTwo = (i: string, d = 256): number => {
-    // p1 way to slow and reaching array max with d = 256
     let f = i.split(",").map(Number).reduce((a, p) => {
         a[p] = a[p] + 1
         return a
@@ -25,6 +16,15 @@ const partTwo = (i: string, d = 256): number => {
         a[6] += nf;
         return a
     }, f).reduce((a, c) => a + c, 0);
+    // p1 way too slow and reaching array max with d = 256
+    // const f = i.split(",").map(Number)
+    // return [...Array(d).keys()].reduce((a, i) => {
+    //     const z = a.filter(n => n === 0).length
+    //     return [...a.map(n => n === 0 ? 6 : n - 1), ...(Array(z).fill(8))]
+    // }, f).length
+}
+const partTwo = (i: string, d = 256): number => {
+    return partOne(i, d)
 }
 
 // --- Tests
@@ -35,8 +35,7 @@ partOneEnabled && testsEnabled && (() => {
 })()
 
 partTwoEnabled && testsEnabled && (() => {
-    assertEquals(partTwo('3,4,3,1,2', 18), 26)
-    assertEquals(partTwo('3,4,3,1,2', 80), 5934)
+    // assertEquals(partTwo(''), 0)
 })()
 
 // --- End Tests
