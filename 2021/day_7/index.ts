43db33fc4558dcacef4aa50309dcebd74289b8af
Miguel Korn <miguel.korn@hva.nl>
2021-12-10 05:57:09 +0100

    [20201:7] cleanup

--- 2021/day_7/index.ts
+++ 2021/day_7/index.ts
@@ -4,15 +4,17 @@ import { getEnabledParts, getInput, runAndTime } from "../../utils/utils.ts";
 const [partOneEnabled, partTwoEnabled, testsEnabled] = getEnabledParts()
 const [input, input2] = await getInput()
 
-const partOne = (i: string, n: number = 1000): number => {
+const partOne = (i: string): number => {
     const nums = i.split(",").map(Number);
+    const n = Math.max(...nums);
     const mx: number[] = [...Array(n).keys()].map((i) => nums.map(x => Math.abs(x - i - 1)).reduce((acc, cur) => acc + cur, 0))
     return Math.min(...mx)
 }
-const partTwo = (i: string, n: number = 1000): number => {
+const partTwo = (i: string): number => {
     const nums = i.split(",").map(Number);
+    const n = Math.max(...nums);
     const mx: number[] = [...Array(n).keys()].map((i) => nums.map(x => {
-        n = Math.abs(x - i - 1)
+        const n = Math.abs(x - i - 1)
         return n * (n + 1) / 2
     }).reduce((acc, cur) => acc + cur, 0));
     return Math.min(...mx)
@@ -21,11 +23,11 @@ const partTwo = (i: string, n: number = 1000): number => {
 // --- Tests
 
 partOneEnabled && testsEnabled && (() => {
-    assertEquals(partOne('16,1,2,0,4,2,7,1,2,14', 10), 37)
+    assertEquals(partOne('16,1,2,0,4,2,7,1,2,14'), 37)
 })()
 
 partTwoEnabled && testsEnabled && (() => {
-    assertEquals(partTwo('16,1,2,0,4,2,7,1,2,14', 10), 168)
+    assertEquals(partTwo('16,1,2,0,4,2,7,1,2,14'), 168)
 })()
 
 // --- End Tests
