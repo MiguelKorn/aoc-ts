97fc6055416df7d546a4abc655aab4a52f68f1fa
Miguel Korn <miguel.korn@hva.nl>
2021-12-10 08:06:38 +0100

    [2021:10] cleanup

--- 2021/day_10/index.ts
+++ 2021/day_10/index.ts
@@ -7,63 +7,37 @@ const [input, input2] = await getInput()
 const partOne = (i: string): number => {
     const lines = i.split("\n")
 
-    const nums: number[] = []
-    lines.forEach(c => {
-        while (c.includes('()') || c.includes('[]') || c.includes('{}') || c.includes('<>')) {
-            if (c.includes('()')) c = c.replace(/(\(\))/g, '')
-            if (c.includes('[]')) c = c.replace(/(\[])/g, '')
-            if (c.includes('{}')) c = c.replace(/({})/g, '')
-            if (c.includes('<>')) c = c.replace(/(<>)/g, '')
+    const nums = lines.reduce((a, c) => {
+        const r = /(\(\)|{}|\[]|<>)/g
+        while (c.match(r)) c = c.replace(r, '')
+
+        if (c.match(/([)}\]>])/g)) {
+            const char = c.match(/([)}\]>])/)![0]
+            const i = [')', ']', '}', '>'].findIndex(c => c === char)
+            a[i]++
         }
 
-        if (c.includes(')') || c.includes(']') || c.includes('}') || c.includes('>')) {
-            let char = c.charAt(0)
-            c = c.substring(1)
-            while (char === '(' || char === '[' || char === '{' || char === '<') {
-                char = c.charAt(0)
-                c = c.substring(1)
-            }
+        return a
+    }, new Array(4).fill(0))
 
-            if (char === ')') nums.push(3)
-            if (char === ']') nums.push(57)
-            if (char === '}') nums.push(1197)
-            if (char === '>') nums.push(25137)
-        }
-    })
-
-    const x1 = nums.filter(n => n === 3).length * 3
-    const x2 = nums.filter(n => n === 57).length * 57
-    const x3 = nums.filter(n => n === 1197).length * 1197
-    const x4 = nums.filter(n => n === 25137).length * 25137
-
-    return x1 + x2 + x3 + x4
+    return [3, 57, 1197, 25137].reduce((a, b, i) => a + (b * nums[i]), 0)
 }
 const partTwo = (i: string): number => {
     const lines = i.split("\n")
 
-    const nums: number[] = []
-    lines.forEach((c) => {
-        while (c.includes('()') || c.includes('[]') || c.includes('{}') || c.includes('<>')) {
-            if (c.includes('()')) c = c.replace(/(\(\))/g, '')
-            if (c.includes('[]')) c = c.replace(/(\[])/g, '')
-            if (c.includes('{}')) c = c.replace(/({})/g, '')
-            if (c.includes('<>')) c = c.replace(/(<>)/g, '')
+    const nums = lines.reduce((nums: number[], c) => {
+        const r = /(\(\)|{}|\[]|<>)/g
+        while (c.match(r)) c = c.replace(r, '')
+
+        if (!c.match(/([)}\]>])/g)) {
+            const n = c.split("").reverse().reduce((a, b) => {
+                return a * 5 + (['(', '[', '{', '<'].findIndex(c => c === b) + 1)
+            }, 0)
+            return [...nums, n]
         }
 
-        if (!(c.includes(')') || c.includes(']') || c.includes('}') || c.includes('>'))) {
-            const n = c.split("").reverse().reduce((a, b) => {
-                a *= 5
-                if (b === '(') a += 1
-                if (b === '[') a += 2
-                if (b === '{') a += 3
-                if (b === '<') a += 4
-                return a
-            }, 0)
-            nums.push(n)
-        }
-    })
-    nums.sort((a, b) => a - b)
-    console.log(nums, Math.floor(nums.length / 2))
+        return nums
+    }, []).sort((a, b) => a - b)
     return nums[Math.floor(nums.length / 2)]
 }
 
